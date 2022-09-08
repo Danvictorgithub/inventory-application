63004ce5d1a24e755756559c652f1bd2ef22f35f
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-09-08 16:44:46 +0800

    updated cars Model

--- models/carsModel.js
+++ models/carsModel.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 CarsSchema = new Schema({
-    name: {type: String}
+    name: {type: String},
+    brand: {type:Schema.Types.ObjectId, ref:'Brands', required:true},
+    car_type:{type:Schema.Types.ObjectId, ref:'Cartypes', required:true},
+    description: {type: String},
+    image: {type: String},
 });
+
 module.exports = mongoose.model('Cars', CarsSchema);
