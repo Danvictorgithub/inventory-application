2271d82bc712c0a331415bcc5dd38a95daa63c90
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2022-12-26 19:28:16 +0800

    fixed overflowing image

--- models/carinstance.js
+++ models/carinstance.js
@@ -5,5 +5,8 @@ const CarInstanceSchema = new Schema({
 	car:{type:Schema.Types.ObjectId, ref:"Car",required:true},
 	status: {type:String, required:true,enum:["Available","Out of Stock","Available-SecondHand"],default:"Out of Stock"},
 });
+CarInstanceSchema.virtual("url").get(function(){
+	return `/catalogue/shop/carinstance/${this._id}`;
+});
 
 module.exports = mongoose.model("CarInstance", CarInstanceSchema);
